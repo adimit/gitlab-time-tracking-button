aacbfcf5b54aac28818d02acb2d508ad12a86f3f
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-10 13:14:18 +0200

    Implement event listener for storage updates

--- src/InstanceManager.js
+++ src/InstanceManager.js
@@ -1,21 +1,31 @@
+const fireEvent = (handlers, data) => {
+  handlers.forEach(f => f(data));
+};
+
 export default class InstanceManager {
   constructor(myChrome) {
     this.chrome = myChrome;
     this.instances = {};
+    this.removalHandlers = [];
   }
 
   async updateStorage(changes, namespace) {
-    console.log(changes, namespace);
+    this.instances = changes.gitlabs.newValue;
     // updates instances
     // TODO triggers removed instance event if any instance(s) got removed (need
     // to add registering function for event handler first)
+    return this;
   }
 
   onInstanceRemoval(listener) {
-
+    this.removalHandlers.push(listener);
+    return this;
   }
 
   isRegisteredInstance(url) {
-    return false;
+    const parser = document.createElement('a');
+    parser.href = url;
+    const key = `${parser.protocol}//${parser.host}`;
+    return Object.keys(this.instances).includes(key);
   }
 }
