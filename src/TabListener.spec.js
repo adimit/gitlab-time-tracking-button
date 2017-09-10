d819b39c602ca75e98e35f186075bbe054ece0ac
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-10 21:19:08 +0200

    Add a mock event to test

--- src/TabListener.spec.js
+++ src/TabListener.spec.js
@@ -6,7 +6,9 @@ describe('TabListener', function () {
   describe('updateTabs', function () {
     it('Only reacts on complete events for registered instances', function () {
       const tabMessage = {
-
+        status: 'complete',
+        url: 'https://gitlab.com/some/project/issues/1',
+        id: 99,
       };
     });
   });
