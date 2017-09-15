f376381ffe905fffa109ba8e052a24eac8ee1803
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-15 23:14:09 +0200

    Fix test, sinon was behaving weirdly

--- src/Clock.spec.js
+++ src/Clock.spec.js
@@ -2,25 +2,16 @@
 
 import sinon from 'sinon';
 import sinonTestFactory from 'sinon-test';
-import { expect, describe, it, beforeEach, afterEach } from 'mocha';
+import { expect, describe, it } from 'mocha';
 
 import Clock from './Clock';
 
 const sinonTest = sinonTestFactory(sinon);
 
 describe('Clock', function () {
-  let fakeTime;
-
-  beforeEach(function () {
-    fakeTime = sinon.useFakeTimers();
-  });
-
-  afterEach(function () {
-    fakeTime.restore();
-  });
-
   it('Starts and reports correct time', sinonTest(function () {
     // given
+    const fakeTime = sinon.useFakeTimers();
     const clock = new Clock();
     const seconds = 10;
 
@@ -30,6 +21,7 @@ describe('Clock', function () {
 
     // then
     expect(clock.getTime()).to.equal(seconds);
+    fakeTime.restore();
   }));
 
   it('Starts and stops on toggle', sinonTest(function () {
@@ -46,6 +38,7 @@ describe('Clock', function () {
 
   it('Resumes correctly after pausing', sinonTest(function () {
     // given
+    const fakeTime = sinon.useFakeTimers();
     const clock = new Clock();
     const firstInterval = 5;
     const secondInterval = 7;
@@ -62,11 +55,11 @@ describe('Clock', function () {
     // then
     expect(clock.getTime()).to.equal(firstInterval + thirdInterval);
     const timeLog = clock.getTimeLog();
-    expect(timeLog.length()).to.equal(2);
+    expect(timeLog.length).to.equal(1);
     expect(timeLog[0].duration).to.equal(firstInterval);
-    expect(timeLog[1].duration).to.be(undefined); // haven't stopped the clock yet.
 
     clock.stop();
     expect(clock.getTimeLog()[1].duration).to.equal(thirdInterval);
+    fakeTime.restore();
   }));
 });
