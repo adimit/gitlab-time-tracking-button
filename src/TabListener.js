0e23856143ef9a44051e5bce5f0ec5ba10ad5acb
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-15 19:54:37 +0200

    Add css

--- src/TabListener.js
+++ src/TabListener.js
@@ -1,6 +1,7 @@
-// We import the raw js from Button.js, which is a nasty thing to do.
+// We import the raw js and css from Button.{js,scss}, which is a nasty thing to do.
 // ESlint is going to complain about many things, but we know what we're doing! I think…
 import buttonJs from 'raw-loader!./Button'; // eslint-disable-line
+import buttonCss from 'raw-loader!sass-loader!./Button.scss'; // eslint-disable-line
 
 export default class TabListener {
   constructor(chromeTabs, instanceManager) {
@@ -24,7 +25,8 @@ export default class TabListener {
   }
 
   async insertAssetsInto(tabId, data) {
-    const js = `const issueData = ${JSON.stringify(data)};\n${buttonJs}`;
-    this.chromeTabs.insertJs(tabId, js);
+    const js = `const injectedIssueData = ${JSON.stringify(data)};\n${buttonJs}`;
+    await this.chromeTabs.insertJs(tabId, js);
+    await this.chromeTabs.insertCss(tabId, buttonCss);
   }
 }
