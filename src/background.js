892d951849aa6af3b944c18e6ba827c24022f916
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-15 18:40:57 +0200

    Rewire background, now asks storage for gitlabs

--- src/background.js
+++ src/background.js
@@ -1,10 +1,15 @@
 import TabListener from './TabListener';
 import InstanceManager from './InstanceManager';
 import ChromeAdapter from './ChromeAdapter';
+import ChromeTabs from './ChromeTabs';
 
-const Chrome = new ChromeAdapter(chrome);
-const instanceManager = new InstanceManager(Chrome);
-const tabListener = new TabListener(Chrome, instanceManager);
+const chromeAdapter = new ChromeAdapter(chrome);
+const chromeTabs = new ChromeTabs(chrome);
 
-chrome.storage.onChanged.addListener(changes => instanceManager.updateStorage(changes));
-chrome.tabs.onUpdated.addListener((tabid, changeInfo, data) => tabListener.updateTabs(data));
+chromeAdapter.getOrDefault('gitlabs', { gitlabs: {} }).then(({ gitlabs }) => {
+  const instanceManager = new InstanceManager(gitlabs);
+  const tabListener = new TabListener(chromeTabs, instanceManager);
+
+  chrome.storage.onChanged.addListener(changes => instanceManager.updateStorage(changes));
+  chrome.tabs.onUpdated.addListener((tabid, changeInfo, data) => tabListener.updateTabs(data));
+});
