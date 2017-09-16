7793e832cb92ecf7ab4000fed5f5645e3d4861fe
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-16 15:13:25 +0200

    Implement server

--- src/Server.js
+++ src/Server.js
@@ -1,3 +1,36 @@
+/*
+  curl --request POST --header "PRIVATE-TOKEN: 9koXpg98eAheJpvBs5tK" https://gitlab.example.com/api/v4/projects/5/issues/93/add_spent_time?duration=1h
+*/
+
+/**
+ * Call the server the token as header, expect a json body in return.
+ *
+ * @param {string} url The API endpoint to call
+ * @param {string} token The token
+ *
+ * @return {Promise} Promise containing parsed JSON response
+ */
+const postGitlab = (url, token) => new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('PRIVATE-TOKEN', token);
+  xhr.onload = () => {
+    if (xhr.status === 200 || xhr.status === 201) {
+      try {
+        const obj = JSON.parse(xhr.responseText);
+        resolve(obj);
+      } catch (e) {
+        reject(e);
+      }
+    } else {
+      reject(Error(`${xhr.status}: ${xhr.statusText}`));
+    }
+  };
+  xhr.onerror = () => reject(Error(`Network error. ${xhr.statusText}`));
+  xhr.send();
+});
+
 export default class Server {
   constructor(instanceManager) {
     this.instanceManager = instanceManager;
@@ -5,8 +38,14 @@ export default class Server {
 
   async record(time, location) {
     const key = this.instanceManager.getApiKey(location.instance);
-    console.log('Recording time', time, location, key);
-
-    return { status: 'ok' };
+    const projectId = encodeURIComponent(`${location.group}/${location.project}`);
+    const apiPath = `add_spent_time?duration=${Math.floor(time)}s`;
+    const url = `${location.instance}api/v4/projects/${projectId}/issues/${location.issue}/${apiPath}`;
+    try {
+      const response = await postGitlab(url, key);
+      return { status: 'ok', totalTimeSpent: response.total_time_spent };
+    } catch (e) {
+      return { status: 'error', message: e.message };
+    }
   }
 }
---
+++ src/Server.spec.js
@@ -0,0 +1,58 @@
+/* eslint-disable func-names, prefer-arrow-callback */
+import { beforeEach, expect, describe, it } from 'mocha';
+import sinon from 'sinon';
+import sinonTestFactory from 'sinon-test';
+
+import Server from './Server';
+
+const sinonTest = sinonTestFactory(sinon);
+
+describe('Server API', function () {
+  let xhr;
+  let requests;
+
+  beforeEach(function () {
+    xhr = sinon.useFakeXMLHttpRequest();
+
+    requests = [];
+
+    xhr.onCreate = function (newXhr) {
+      requests.push(newXhr);
+    };
+  });
+
+  it('Makes a successful API call', async function () {
+    const apiKey = 'the api key';
+    const fakeInstaceManager = { getApiKey: () => apiKey };
+    const server = new Server(fakeInstaceManager);
+
+    const resultPromise = server.record(7, {
+      instance: 'https://example.com:5/',
+      group: 'theGroup',
+      issue: '11',
+      project: 'theProject',
+    });
+
+    expect(requests.length).to.equal(1);
+
+    requests[0].respond(
+      200,
+      { 'Content-Type': 'application/json' },
+      JSON.stringify({
+        human_time_estimate: null,
+        human_total_time_spent: '1h',
+        time_estimate: 0,
+        total_time_spent: 3600,
+      }),
+    );
+
+    const response = await resultPromise;
+
+    expect(response.status).to.equal('ok');
+    expect(response.totalTimeSpent).to.equal(3600);
+  });
+
+  it('Returns useful error code on failure', sinonTest(function () {
+    
+  }));
+});
