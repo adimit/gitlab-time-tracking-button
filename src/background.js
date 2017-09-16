b2ad3ccbe80aa65ab52a0b1beb5670b44dc2d192
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-16 20:17:32 +0200

    Work around yet more bullshit

--- src/background.js
+++ src/background.js
@@ -3,16 +3,23 @@ import InstanceManager from './InstanceManager';
 import ChromeAdapter from './ChromeAdapter';
 import ChromeTabs from './ChromeTabs';
 import TimeKeeper from './TimeKeeper';
+import isFirefox from './UserAgent';
 
 const chromeAdapter = new ChromeAdapter(chrome);
 const chromeTabs = new ChromeTabs(chrome);
 const timeKeeper = new TimeKeeper(chromeAdapter);
 
-chrome.runtime.onMessage.addListener(
-  async (message, sender, sendResponse) => {
-    await timeKeeper.processMessage(message, sender, sendResponse);
-    return true;
-  });
+if (isFirefox()) {
+  chrome.runtime.onMessage.addListener(
+    async (message, sender, sendResponse) =>
+      timeKeeper.processMessage(message, sender, sendResponse));
+} else {
+  chrome.runtime.onMessage.addListener(
+    (message, sender, sendResponse) => {
+      timeKeeper.processMessage(message, sender, sendResponse);
+      return true;
+    });
+}
 
 InstanceManager.initialize(chromeAdapter).then((instanceManager) => {
   const tabListener = new TabListener(chromeTabs, instanceManager);
