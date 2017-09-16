c2a3bade5634da91bf1d4704aeaabbe5698904e1
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-16 17:25:31 +0200

    Implement TimeKeeper

--- src/TimeKeeper.js
+++ src/TimeKeeper.js
@@ -1,10 +1,36 @@
+const makeKey = ({ instance, group, project, issue }) => `${instance}&${group}&${project}&${issue}`;
+
 export default class TimeKeeper {
   constructor(chrome) {
     this.chrome = chrome;
+    this.clocks = {};
   }
 
   async processMessage(message, sender) {
-    console.log('got message', message, sender);
-    return { answer: 'got the message!' };
+    const { action, issueData, clockData } = message;
+    const key = makeKey(issueData);
+    switch (action) {
+      case 'update': return this.updateClock(key, clockData);
+      case 'trash': return this.trashClock(key);
+      case 'get': return this.giveClock(key);
+      default: throw Error(`Unknown action ${action}`);
+    }
+  }
+
+  async updateClock(key, clockData) {
+    this.clocks[key] = clockData;
+    await this.chrome.set({ clocks: this.clocks });
+    console.log('clocks', this.clocks);
+  }
+
+  async trashClock(key) {
+    this.clocks[key] = null;
+    await this.chrome.set({ clocks: this.clocks });
+    console.log('clocks', this.clocks);
+  }
+
+  async giveClock(key) {
+    const { clocks } = await this.chrome.getOrDefault('clocks', { clocks: {} });
+    return clocks[key] || null;
   }
 }
