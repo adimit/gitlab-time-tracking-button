e58aa6bfae68d7201aec651237c8afc2941f58e3
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-10-02 22:02:12 +0200

    Prevent double insertion of clock (#11)

--- src/content_scripts/ClockButton.js
+++ src/content_scripts/ClockButton.js
@@ -7,96 +7,101 @@ import ChromeAdapter from '../ChromeAdapter';
 import InstanceManager from '../InstanceManager';
 import PostOffice from './PostOffice';
 
-const urlParser = new UrlParser(window.location.href);
-const issueData = urlParser.getAllData();
-const postOffice = new PostOffice(chrome, issueData);
+const clockContainerId = 'time-tracking-clock-container';
 
-const startStopButton = document.createElement('div');
-startStopButton.classList.add('start-stop-button');
-startStopButton.textContent = 'start';
+if (document.getElementById(clockContainerId) === null) {
+  const urlParser = new UrlParser(window.location.href);
+  const issueData = urlParser.getAllData();
+  const postOffice = new PostOffice(chrome, issueData);
 
-const saveButton = document.createElement('div');
-saveButton.classList.add('save-time-button');
-saveButton.classList.add('green');
-saveButton.textContent = 'save';
+  const startStopButton = document.createElement('div');
+  startStopButton.classList.add('start-stop-button');
+  startStopButton.textContent = 'start';
 
-const timeDisplay = document.createElement('span');
-timeDisplay.classList.add('time-display');
+  const saveButton = document.createElement('div');
+  saveButton.classList.add('save-time-button');
+  saveButton.classList.add('green');
+  saveButton.textContent = 'save';
 
-const trashButton = document.createElement('div');
-trashButton.classList.add('trash-time-button');
-trashButton.classList.add('red');
-trashButton.textContent = 'trash';
+  const timeDisplay = document.createElement('span');
+  timeDisplay.classList.add('time-display');
 
-const ourContainer = document.createElement('div');
-ourContainer.classList.add('block');
-ourContainer.classList.add('clock_container');
+  const trashButton = document.createElement('div');
+  trashButton.classList.add('trash-time-button');
+  trashButton.classList.add('red');
+  trashButton.textContent = 'trash';
 
-ourContainer.append(timeDisplay);
-ourContainer.append(startStopButton);
-ourContainer.append(saveButton);
-ourContainer.append(trashButton);
+  const ourContainer = document.createElement('div');
+  ourContainer.setAttribute('id', clockContainerId);
+  ourContainer.classList.add('block');
+  ourContainer.classList.add('clock_container'); // gitlab does it this way…
 
-const dueDateContainer = document.querySelector('.block.due_date');
-dueDateContainer.before(ourContainer);
+  ourContainer.append(timeDisplay);
+  ourContainer.append(startStopButton);
+  ourContainer.append(saveButton);
+  ourContainer.append(trashButton);
 
-postOffice.getClock().then((savedClock) => {
-  const clockView = new ClockViewModel(new Clock(savedClock));
+  const dueDateContainer = document.querySelector('.block.due_date');
+  dueDateContainer.before(ourContainer);
 
-  clockView.subscribe((rawTime) => {
-    timeDisplay.textContent = DateFormat.precise(rawTime);
-  });
+  postOffice.getClock().then((savedClock) => {
+    const clockView = new ClockViewModel(new Clock(savedClock));
 
-  clockView.onStart(() => {
-    startStopButton.textContent = 'stop';
-    startStopButton.classList.remove('stopped');
-    startStopButton.classList.add('started');
-    saveButton.classList.remove('invisible');
-    trashButton.classList.remove('invisible');
-  });
+    clockView.subscribe((rawTime) => {
+      timeDisplay.textContent = DateFormat.precise(rawTime);
+    });
 
-  clockView.onStop(() => {
-    startStopButton.textContent = 'start';
-    startStopButton.classList.remove('started');
-    startStopButton.classList.add('stopped');
-  });
-
-  clockView.onChangeState(async (rawTime) => {
-    timeDisplay.textContent = DateFormat.precise(rawTime);
-    if (rawTime > 0) {
+    clockView.onStart(() => {
+      startStopButton.textContent = 'stop';
+      startStopButton.classList.remove('stopped');
+      startStopButton.classList.add('started');
       saveButton.classList.remove('invisible');
       trashButton.classList.remove('invisible');
-    } else {
-      saveButton.classList.add('invisible');
-      trashButton.classList.add('invisible');
-    }
-  });
+    });
 
-  InstanceManager.initialize(new ChromeAdapter(chrome)).then((instanceManager) => {
-    const server = new Server(instanceManager);
+    clockView.onStop(() => {
+      startStopButton.textContent = 'start';
+      startStopButton.classList.remove('started');
+      startStopButton.classList.add('stopped');
+    });
 
-    startStopButton.onclick = () => {
-      clockView.toggle();
-      postOffice.updateClock(clockView.getClock());
-    };
+    clockView.onChangeState(async (rawTime) => {
+      timeDisplay.textContent = DateFormat.precise(rawTime);
+      if (rawTime > 0) {
+        saveButton.classList.remove('invisible');
+        trashButton.classList.remove('invisible');
+      } else {
+        saveButton.classList.add('invisible');
+        trashButton.classList.add('invisible');
+      }
+    });
 
-    trashButton.onclick = () => {
-      clockView.resetClock(new Clock());
-      postOffice.trashClock();
-    };
+    InstanceManager.initialize(new ChromeAdapter(chrome)).then((instanceManager) => {
+      const server = new Server(instanceManager);
 
-    saveButton.onclick = async () => {
-      const time = clockView.getTime();
-      clockView.stop();
-      const response = await server.record(time, issueData);
-      if (response.status === 'ok') {
+      startStopButton.onclick = () => {
+        clockView.toggle();
+        postOffice.updateClock(clockView.getClock());
+      };
+
+      trashButton.onclick = () => {
         clockView.resetClock(new Clock());
         postOffice.trashClock();
-      } else {
-        console.error(response); // eslint-disable-line no-console
-      }
-    };
-  });
+      };
 
-  clockView.changeState();
-});
+      saveButton.onclick = async () => {
+        const time = clockView.getTime();
+        clockView.stop();
+        const response = await server.record(time, issueData);
+        if (response.status === 'ok') {
+          clockView.resetClock(new Clock());
+          postOffice.trashClock();
+        } else {
+          console.error(response); // eslint-disable-line no-console
+        }
+      };
+    });
+
+    clockView.changeState();
+  });
+}
