88a24e18f1e58ffed92944a24759f8bd82db4f53
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-10-04 09:08:22 +0200

    Use different manifests for chrome & FF

--- webpack.config.js
+++ webpack.config.js
@@ -23,94 +23,98 @@ const CssTextPlugin = new ExtractTextPlugin("[name].css");
 const addon = path.resolve(__dirname, "addon");
 const source_path = path.resolve(__dirname, "./src");
 
-module.exports = {
-  entry: {
-    /** background script */
-    'background': path.resolve(source_path, "background.js"),
 
-    /** options script and its assets, used for addon options.
-        The HTML is extracted with the extract text plugin */
-    'options': glob.sync(path.resolve(source_path, "options.*")),
+module.exports = env => {
+  const variant = env.TARGET || "firefox";
+  return {
+    entry: {
+      /** background script */
+      'background': path.resolve(source_path, "background.js"),
 
-    'content_scripts/ClockButton': glob.sync(path.resolve(source_path, "content_scripts/ClockButton.*"))
-  },
-  devtool: 'source-map',
-  output: {
-    path: addon,
-    filename: "[name].js"
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      /** Copies the icons and the manifest to the target location */
-      {
-        from: path.resolve(__dirname, "manifest.json"),
-        to:   path.resolve(addon, "manifest.json")
-      },
-      {
-        from: path.resolve(__dirname, "icons"),
-        to:   path.resolve(addon, "icons")
-      }
-    ]),
-    HtmlTextPlugin,
-    CssTextPlugin
-  ],
-  resolve: {
-    /** This hack is here because sinon uses 'require' in buggy ways */
-    alias: {
-      sinon: path.resolve(__dirname, 'node_modules/sinon/pkg/sinon.js') // require dist version instead
-    }
-  },
-  module: {
-    /** See above comment on resolve.alias.sinon */
-    noParse: [ /node_modules\/sinon$/ ],
+      /** options script and its assets, used for addon options.
+          The HTML is extracted with the extract text plugin */
+      'options': glob.sync(path.resolve(source_path, "options.*")),
 
-    rules: [
-      {
-        /** run all js files through babel loader with es2015, except node_modules */
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015'],
-            plugins: ['transform-runtime']
-          }
+      'content_scripts/ClockButton': glob.sync(path.resolve(source_path, "content_scripts/ClockButton.*"))
+    },
+    devtool: 'source-map',
+    output: {
+      path: addon,
+      filename: "[name].js"
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        /** Copies the icons and the manifest to the target location */
+        {
+          from: path.resolve(__dirname, `manifest-${variant}.json`),
+          to:   path.resolve(addon, "manifest.json")
+        },
+        {
+          from: path.resolve(__dirname, "icons"),
+          to:   path.resolve(addon, "icons")
         }
-      },
-      {
-        test: /assets.*\.css$/,
-        loader: "css-loader"
-      },
-      {
-        /** The html file for options is extracted separately, as it needs
-            to be present for the webextensions options mechanism to work.*/
-        test: /.*(options).*\.html$/,
-        loader: HtmlTextPlugin.extract({use: "raw-loader"})
-      },
-      {
-        /** The css files are also extracted separately */
-        test: /\.scss$/,
-        loader: CssTextPlugin.extract({use: "css-loader!sass-loader"})
-      },
-      {
-        /** HTML files are included in the JS bundle, and not extracted. */
-        test: /assets.*\.html$/,
-        loader: "html-loader"
-      },
-      {
-        /** PNGs are loaded inline in JS bundles */
-        test: /.*\.png$/,
-        loader: "url-loader?mimetype=image/png"
-      },
-      {
-        /** GIFs are loaded inline in JS bundles */
-        test: /.*\.gif$/,
-        loader: "url-loader?mimetype=image/gif"
+      ]),
+      HtmlTextPlugin,
+      CssTextPlugin
+    ],
+    resolve: {
+      /** This hack is here because sinon uses 'require' in buggy ways */
+      alias: {
+        sinon: path.resolve(__dirname, 'node_modules/sinon/pkg/sinon.js') // require dist version instead
       }
-    ]
-  },
-  node: {
-    fs: 'empty',
-    child_process: 'empty'
-  }
+    },
+    module: {
+      /** See above comment on resolve.alias.sinon */
+      noParse: [ /node_modules\/sinon$/ ],
+
+      rules: [
+        {
+          /** run all js files through babel loader with es2015, except node_modules */
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+              plugins: ['transform-runtime']
+            }
+          }
+        },
+        {
+          test: /assets.*\.css$/,
+          loader: "css-loader"
+        },
+        {
+          /** The html file for options is extracted separately, as it needs
+              to be present for the webextensions options mechanism to work.*/
+          test: /.*(options).*\.html$/,
+          loader: HtmlTextPlugin.extract({use: "raw-loader"})
+        },
+        {
+          /** The css files are also extracted separately */
+          test: /\.scss$/,
+          loader: CssTextPlugin.extract({use: "css-loader!sass-loader"})
+        },
+        {
+          /** HTML files are included in the JS bundle, and not extracted. */
+          test: /assets.*\.html$/,
+          loader: "html-loader"
+        },
+        {
+          /** PNGs are loaded inline in JS bundles */
+          test: /.*\.png$/,
+          loader: "url-loader?mimetype=image/png"
+        },
+        {
+          /** GIFs are loaded inline in JS bundles */
+          test: /.*\.gif$/,
+          loader: "url-loader?mimetype=image/gif"
+        }
+      ]
+    },
+    node: {
+      fs: 'empty',
+      child_process: 'empty'
+    }
+  };
 };
