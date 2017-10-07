91152db5564cc86042e85fadb8b10f708a8df10e
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-10-07 16:56:06 +0200

    Stub out TabRegistry behaviour

--- src/TabRegistry.js
+++ src/TabRegistry.js
@@ -1,21 +1,30 @@
 export default class TabRegistry {
   constructor(browser) {
     this.browser = browser;
+    this.tabs = new Map();
+    this.issues = new Map();
   }
 
   update(tabId, issueData, clockData) {
-    console.log('update', tabId, issueData, clockData);
+    // find all tabs for issue
+    // send update clock data to those tabs
+  }
+
+  getTabsForIssue(issueData, originalTab) {
+    // get all tabs containing this issueData
+    // remove originalTab from the set
   }
 
   trash(tabId, issueData) {
-    console.log('trash', tabId, issueData);
+    // find all tabs for issue
+    // send trash message to those tabs
   }
 
-  registerTab(tabId) {
-    console.log('registering', tabId);
+  registerTab({ tabId, issueData }) {
+    this.tabs.set(tabId, issueData);
   }
 
   deregisterTab(tabId) {
-    console.log('deregistering', tabId);
+    this.tabs.delete(tabId);
   }
 }
